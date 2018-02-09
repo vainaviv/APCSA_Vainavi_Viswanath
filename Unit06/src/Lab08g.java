2eaa4f1e3db8f6c04d56baa5dbaf08fa290cbca8
vainaviv <32180983+vainaviv@users.noreply.github.com>
2018-02-09 09:30:15 -0800

    Started unit 6

---
+++ Unit06/src/Lab08g.java
@@ -0,0 +1,28 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		LoopStats test = new LoopStats();
+	
+		test.setNums(1,5);
+		System.out.println(test);
+		
+		test.setNums(2,8);
+		System.out.println(test);
+		
+		test.setNums(5,15);
+		System.out.println(test);
+		
+					
+	}
+}
