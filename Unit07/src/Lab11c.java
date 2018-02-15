8d094890bbbf463cbfd924eeaaf8397d0682354f
vainaviv <32180983+vainaviv@users.noreply.github.com>
2018-02-15 09:30:09 -0800

    Worked on unit 7

---
+++ Unit07/src/Lab11c.java
@@ -0,0 +1,37 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+import java.util.Scanner;
+
+public class Lab11c
+{
+   public static void main( String args[] )
+   {
+	   TriangleThree test = new TriangleThree();
+	   
+	   test.setTriangle("A", 3);
+	   System.out.println(test);
+	   
+	   test.setTriangle("X", 7);
+	   System.out.println(test);
+	   
+	   test.setTriangle("R", 1);
+	   System.out.println(test);
+	   
+	   test.setTriangle("T", 5);
+	   System.out.println(test);
+	   
+	   test.setTriangle("W", 4);
+	   System.out.println(test);
+
+
+
+
+
+	}
+}
